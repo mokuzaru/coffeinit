c1156a0423d4e6b8fe58745176828a59f7a490ac
mokuzaru <kenny.otoya@pucp.pe>
2017-06-18 22:39:43 -0500

    first assigment

---
+++ lesson1/twoLineHello/TwoLineHelloWorld.java
@@ -0,0 +1,12 @@
+// Look for lines that begin with two slashes! These are "comments" and 
+// we will use them to let you know what you should do on each programming
+// exercise. Have fun!
+
+public class TwoLineHelloWorld
+{
+    public static void main(String args[])
+    {
+       System.out.println(3+4+5);
+       System.out.println("3+4+5");
+    }
+}
